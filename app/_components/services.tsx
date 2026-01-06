a534e27546b594226476f3438e3c9c293b7c4f9b
Samuel Lima <samucasilvalima@gmail.com>
2026-01-06 13:05:58 -0300

    feat: adicionar funcionalidade de rolagem para os serviços na seção de serviços

--- app/_components/services.tsx
+++ app/_components/services.tsx
@@ -63,52 +63,76 @@ const Services = () => {
     },
   });
 
+  function scrollPrev() {
+    emblaApi?.scrollPrev();
+  }
+
+  function scrollNext() {
+    emblaApi?.scrollNext();
+  }
+
   return (
     <section className="bg-white py-16">
       <div className="container px-4 mx-auto">
         <h2 className="text-4xl">Serviços</h2>
       </div>
-      <div className="overflow-hidden" ref={emblaRef}>
-        <div className="flex">
-          {service.map((item, index) => (
-            <div
-              key={index}
-              className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3"
-            >
-              <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
-                <div className="flex-1 flex items-start justify-between">
-                  <div className="flex gap-3">
-                    <span className="text-3xl">{item.icon}</span>
-                    <div>
-                      <h3 className="font-bold text-xl my-1">{item.title}</h3>
-                      <p className="text-gray-400 text-sm select-none">
-                        {item.description}
-                      </p>
+      <div className="relative">
+        <div className="overflow-hidden" ref={emblaRef}>
+          <div className="flex">
+            {service.map((item, index) => (
+              <div
+                key={index}
+                className="flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3"
+              >
+                <article className="bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col">
+                  <div className="flex-1 flex items-start justify-between">
+                    <div className="flex gap-3">
+                      <span className="text-3xl">{item.icon}</span>
+                      <div>
+                        <h3 className="font-bold text-xl my-1">{item.title}</h3>
+                        <p className="text-gray-400 text-sm select-none">
+                          {item.description}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
 
-                <div className="border-t border-gray-700 pt-4 flex items-center justify-between">
-                  <div className="flex items-center gap-2 text-sm">
-                    <Clock className="w-4 h-4" />
-                    <span>{item.duration}</span>
+                  <div className="border-t border-gray-700 pt-4 flex items-center justify-between">
+                    <div className="flex items-center gap-2 text-sm">
+                      <Clock className="w-4 h-4" />
+                      <span>{item.duration}</span>
+                    </div>
+
+                    <a
+                      target="_blank"
+                      href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações sbore ${item.title}`}
+                      className="flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300"
+                    >
+                      <WhatsappLogoIcon className="w-5 h-5" />
+                      Entrar em contato
+                    </a>
                   </div>
-
-                  <a
-                    target="_blank"
-                    href={`https://wa.me/556799998800?text=Olá vim pelo site e gostaria de mais informações sbore ${item.title}`}
-                    className="flex items-center justify-center gap-2 hover:bg-red-500 px-4 py-1 rounded-md duration-300"
-                  >
-                    <WhatsappLogoIcon className="w-5 h-5" />
-                    Entrar em contato
-                  </a>
-                </div>
-              </article>
-            </div>
-          ))}
+                </article>
+              </div>
+            ))}
+          </div>
         </div>
+
+        <button
+          className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+          onClick={scrollPrev}
+        >
+          <ChevronLeft className="w-6 h-6 text-gray-600" />
+        </button>
+
+        <button
+          className="bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10"
+          onClick={scrollNext}
+        >
+          <ChevronRight className="w-6 h-6 text-gray-600" />
+        </button>
       </div>
-      <div className="relative"></div>
+      <div></div>
     </section>
   );
 };
